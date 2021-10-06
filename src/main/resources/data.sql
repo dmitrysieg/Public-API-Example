23b2749d9273596f1a9c56acd44204a4bf72911f
d.boldyrev <d.boldyrev@platformaofd.ru>
2021-10-06 14:46:13 +0300

    First files

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+drop table if exists cheque;
+
+create table cheque (
+    c_id int auto_increment primary key,
+    c_date timestamp not null,
+    c_total_sum int not null,
+    c_pos_cnt int not null
+);
+
+insert into cheque
+(c_date, c_total_sum, c_pos_cnt)
+values
+    ({ts '2020-09-01 09:00:05.00'}, 250.00, 5),
+    ({ts '2020-10-17 18:15:51.69'}, 1089.00, 1),
+    ({ts '2012-10-18 19:16:22.81'}, 18000.00, 1);
